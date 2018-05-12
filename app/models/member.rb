f29e087db9fd46ed5498160b8f16374d42dcab0c
Nikhil Shahi <shniks@gmail.com>
2018-05-12 17:16:01 -0600

    Created Member PORO

---
+++ app/models/member.rb
@@ -0,0 +1,8 @@
+class Member
+  def initialize(attributes)
+    @name       = attributes[:name]
+    @role       = attributes[:name]
+    @districut  = attributes[:name]
+    @party      = attributes[:name]
+  end
+end
